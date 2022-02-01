5623784afad5ccf5bb66945de5391503f50c2e01
クレイ <83293295+KureiLe@users.noreply.github.com>
2022-02-01 17:06:49 +0700

    Save but still broken \

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from turtle import pos
 import disnake
 from disnake.ext import commands
 from disnake.utils import find
 import json
 from pymongo import MongoClient
+import asyncio
 
 bot = commands.Bot(command_prefix="$")
 json_file = open("config.json").read()
@@ -17,8 +19,7 @@ async def on_ready():
     bot_id = bot.user.id
     print(f"logged in as {bot_id}")
 
-@bot.command()
-async def register(ctx):
+async def check_if_registered(ctx):
     server_id = str(ctx.message.guild.id)
     find = col.find_one({server_id:{"$exists":True}})
     
@@ -27,9 +28,46 @@ async def register(ctx):
             server_id: []
         }
         col.insert_one(post)
-        await ctx.send('Registered')
+
+@bot.command()
+async def whitelists(ctx):
+    await check_if_registered(ctx)
+    server_id = str(ctx.message.guild.id)
+    find = col.find({server_id:{"$exists":True}})
+    for x in find:
+        array = x[server_id]
+        await ctx.send(array)
+
+@bot.command()
+async def whitelist(ctx, value = ''):
+    await check_if_registered(ctx)
+    if value == '':
+        await ctx.send('Command needs an argument (id)')
     else:
-        await ctx.send('Already registered')
+        server_id = str(ctx.message.guild.id)
+        find = col.find({server_id:{"$exists":True}})
+        for x in find:
+            array = x[server_id]
+            original_data = {server_id: array}
+            if value in array:
+                await ctx.send('Already whitelisted')
+            else:
+                async def check():
+                    try:
+                        bot.get_user(value)
+                        return True
+                    except:
+                        return False
+                
+                if await check() == True:
+                    array.append(value)
+                    post = {
+                        "$set": {server_id: array}
+                    }
+                    col.update_one(original_data, post)
+                    await ctx.send('Whitelisted')
+                else:
+                    await ctx.send('Argument must be an id')
 
 discord_key = read_json["discord_key"]
 bot.run(discord_key)
