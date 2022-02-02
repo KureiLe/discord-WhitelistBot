ab03a0eca9bf6cdd8e812e7da379c5bafd22931c
クレイ <83293295+KureiLe@users.noreply.github.com>
2022-02-02 13:51:24 +0700

    added things but borken

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import disnake
+from disnake.ext.commands import has_permissions, MissingPermissions
 from disnake.ext import commands
 from disnake.utils import find
 import json
 from pymongo import MongoClient
 import asyncio
 
-bot = commands.Bot(command_prefix="$")
+intents = disnake.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="$", intents=intents, help_command=None)
 json_file = open("config.json").read()
 read_json = json.loads(json_file)
 
@@ -21,24 +24,65 @@ async def on_ready():
 async def check_if_registered(ctx):
     server_id = str(ctx.message.guild.id)
     find = col.find_one({server_id:{"$exists":True}})
-    
     if find == None:
         post = {
             server_id: []
         }
         col.insert_one(post)
 
-@bot.command()
-async def whitelists(ctx):
-    await check_if_registered(ctx)
-    server_id = str(ctx.message.guild.id)
+async def check_if_registered_event(member):
+    server_id = str(member.guild.id)
+    find = col.find_one({server_id: {"$exists": True}})
+    if find == None:
+        post = {
+            server_id: []
+        }
+        col.insert_one(post)
+
+@bot.event
+async def on_member_join(member):
+    await check_if_registered_event(member)
+    user_id = member.id
+    server_id = str(member.guild.id)
     find = col.find({server_id:{"$exists":True}})
     for x in find:
-        array = x[server_id]
-        await ctx.send(array)
+        id_array = x[server_id]
+    if user_id not in id_array:
+        await member.send('You are not whitelisted. Ask owner/mods for permission to enter the server, and then rejoin')
+        await member.guild.kick(member, reason='Not Whitelisted')
 
 @bot.command()
-async def whitelist(ctx, value = ''):
+async def help(ctx):
+    embed = disnake.Embed(title="Help Commands", color=0xF3FF47)
+    embed.add_field(name="list", value="Shows list of whitelisted iD", inline=True)
+    embed.add_field(name="whitelist", value="Whitelists ID", inline=True)
+    embed.add_field(name="unwhitelist", value="Unwhitelists ID", inline=True)
+    embed.set_footer(text="prefix = '$'")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def list(ctx):
+    await check_if_registered(ctx)
+    server_id = str(ctx.message.guild.id)
+    find = col.find({server_id: {"$exists": True}})
+    for x in find:
+        id_array = x[server_id]
+    msg = ''
+    for x in id_array:
+        username = await bot.fetch_user(x)
+        msg += f'{username} - {x}\n'
+    await ctx.send(msg)
+
+async def check(value):
+    a = await bot.fetch_user(value)
+    if a != None:
+        return True
+    else:
+        return False
+
+@bot.command()
+@has_permissions(administrator=True)
+async def whitelist(ctx, value=''):
     await check_if_registered(ctx)
     if value == '':
         await ctx.send('Command needs an argument (id)')
@@ -51,14 +95,7 @@ async def whitelist(ctx, value = ''):
         if value in array:
             await ctx.send('Already whitelisted')
         else:
-            async def check():
-                a = await bot.fetch_user(value)
-                if a != None:
-                    return True
-                else:
-                    return False
-            
-            if await check() == True:
+            if await check(value) == True:
                 array.append(value)
                 post = {
                     "$set": {server_id: array}
@@ -67,6 +104,39 @@ async def whitelist(ctx, value = ''):
                 await ctx.send('Whitelisted')
             else:
                 await ctx.send('Argument must be an id')
+@whitelist.error
+async def whitelist_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send('Need Administrator Perms')
+
+@bot.command()
+@has_permissions(administrator=True)
+async def unwhitelist(ctx, value=''):
+    await check_if_registered(ctx)
+    if value == '':
+        await ctx.send('Command needs an argument (id)')
+    else:
+        server_id = str(ctx.message.guild.id)
+        find = col.find({server_id: {"$exists": True}})
+        for x in find:
+            array = x[server_id]
+            original_data = {server_id: list(array)}
+        if value not in array:
+            await ctx.send('Already not whitelisted')
+        else:
+            if await check(value) == True:
+                array.remove(value)
+                post = {
+                    "$set": {server_id: array}
+                }
+                col.update_one(original_data, post)
+                await ctx.send('Unwhitelisted')
+            else:
+                await ctx.send('Argument must be an id')
+@unwhitelist.error
+async def unwhitelist_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send('Need Administrator Perms')
 
 discord_key = read_json["discord_key"]
 bot.run(discord_key)
